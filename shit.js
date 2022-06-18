42fd457434dfa3815d2769f74ccac546a5a348ef
Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>
2022-06-18 23:42:48 +0200

    Add files via upload

--- shit.js
+++ shit.js
@@ -3,14 +3,9 @@ document.addEventListener("DOMContentLoaded", dienow);
 function dienow( ){
     
     setInterval(() => {
-    var today = new Date();
+    var today = new Date().toLocaleTimeString();
+    document.getElementById('hours').textContent=today
 
-    var hr = today.getHours()
-    var mn = today.getMinutes()
-    var sc = today.getSeconds()
-    document.getElementById('hours').textContent=hr
-    document.getElementById('minutes').textContent=mn
-    document.getElementById('seconds').textContent=sc
     }, 1);
   }
   
